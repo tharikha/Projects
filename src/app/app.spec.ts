0e2ffc44d409fd82b855f17bab19b0c37a661d8a
Tharikha <159229097+tharikha@users.noreply.github.com>
2026-04-02 14:41:46 +0530

    Add files via upload

---
+++ src/app/app.spec.ts
@@ -0,0 +1,27 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { FormsModule } from '@angular/forms';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent, TaskComponent],
+      imports: [FormsModule]
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent)
+      .toContain('My Task App');
+  });
+});
