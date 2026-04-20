d0bffcc21bd33d39f2d5d8f06970bb1adef1af03
Tharikha <159229097+tharikha@users.noreply.github.com>
2026-04-20 22:15:05 +0530

    Delete backend directory

--- backend/config/connectDatabase.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectDatabase  = () =>{
-     mongoose.connect(process.env.DB_URL).then((con) =>{
-        console.log('MongoDB connected to host:'+con.connection.host)
-     })
-};
-
-module.exports = connectDatabase; 
